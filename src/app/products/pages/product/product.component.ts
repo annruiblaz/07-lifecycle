9a49232bf685bbe6699286733a4793f98307dd31
annruiblaz <anruiblaz@gmail.com>
2025-01-14 13:40:30 +0100

    implementar ciclos de vida + ngOnChanges con componente hijo

--- src/app/products/pages/product/product.component.ts
+++ src/app/products/pages/product/product.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'products-product-page',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
+
+  public isProductVisible: boolean = false;
+  public currentPrice: number = 10;
+
+  constructor() { 
+    console.log('Estoy en el constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('Estoy en ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({changes})
+    console.log('Estoy en ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('Estoy en ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('Estoy en ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('Estoy en ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Estoy en ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('Estoy en ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('Estoy en ngOnDestroy');
+  }
+
+  increasePrice() {
+    this.currentPrice++;
+  }
 
 }
